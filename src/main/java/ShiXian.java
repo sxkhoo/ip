8729b3bf60b959737fbc209bfd1135f62eacc7c1
sxkhoo <khooshixian@gmail.com>
2025-02-05 02:33:43 +0800

    Level-2

--- src/main/java/ShiXian.java
+++ src/main/java/ShiXian.java
@@ -10,9 +10,11 @@ public class ShiXian {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
         Scanner scanner = new Scanner(System.in);
+        String[] storedList = new String[100];
         System.out.println("Hello! I'm Shi Xian.");
         System.out.println("What can I do for you?");
         System.out.println("---------------------------------");
+        int k =0;
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
@@ -20,8 +22,16 @@ public class ShiXian {
                 System.out.println("---------------------------");
                 break;
             }
+            else if (userInput.equals("list")){
+                for(int i = 0; i < k; i++){
+                    System.out.println(i+1+":"+storedList[i]);
+
+                }
+            }
+            storedList[k] = userInput;
+            k++;
             System.out.println("---------------------------");
-            System.out.println(userInput);
+            System.out.println("added: "+userInput);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
